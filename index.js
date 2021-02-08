50265733c751e38ee0a544b8c300c2ff78dd174e
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-02-08 11:08:57 -0500

    Finished lab.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", textChange);
+
+function textChange() {
+  let element = document.querySelector('p');
+  element.textContent = "This is really cool!";
+};
